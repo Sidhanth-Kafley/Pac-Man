8e125ed3c3da2179b6ad7f941eb557cc2fc3f59f
Michelle Wehrle <mwehrlevt@gmail.com>
2020-10-26 16:11:12 -0400

    Worked on ghost class

--- ghost.py
+++ ghost.py
@@ -1,10 +1,30 @@
-class Ghost:
+import pygame
+
+
+class Ghost(pygame.sprite.Sprite):
+    hitPacMan = False
+
     def __init__(self, color):
+        # initialize super class
+        super(Ghost, self).__init__()
         self.color = color
+
+        # need to set the size of the images
+
+    def update(self):
+        pass
 
     # getters
     def getGhostColor(self):
         return self.color
+
+    def determineGhostSpites(self):
+        ghostColor = self.color
+        # determine what images to open based on the color of the ghost
+        GHOST_DOWN = pygame.image.load(ghostColor + 'Ghost_Down.png').convert_alpha()
+        GHOST_LEFT = pygame.image.load(ghostColor + 'Ghost_Left.png').convert_alpha()
+        GHOST_RIGHT = pygame.image.load(ghostColor + 'Ghost_Right.png').convert_alpha()
+        GHOST_UP = pygame.image.load(ghostColor + 'Ghost_Up.png').convert_alpha()
 
     # ghost hits pac-man
     def touchesPacMan(self):
@@ -16,6 +36,14 @@ class Ghost:
         pass
 
     # ghosts randomly moving in maze
-    def ghostMovements(self):
-        pass
+    def ghostMovements(self, movement):
+        # determine what direction the ghost is moving and choose the correct image
+        if movement == 'xRight':
+            pass
+        elif movement == 'xLeft':
+            pass
+        elif movement == 'yUp':
+            pass
+        elif movement == 'yDown':
+            pass
 
